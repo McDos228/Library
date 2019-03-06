fa6d4c3164bd2afd9c1df26199ce233d051d691c
Vdovenko_Stanislav <stanislav.vdov@computools.com>
2019-03-06 10:58:21 +0200

    [auth] db structure config

--- config/index.js
+++ config/index.js
@@ -1,3 +1,13 @@
 module.exports = {
-    port: process.env.PORT || 8081
+    port: process.env.PORT || 8081,
+    secret : 'cool-library-secret-key',
+    env: process.env.NODE_ENV || 'development',
+    database: {
+        username: process.env.DB_USERNAME || "postgres",
+        password: process.env.DB_PASSWORD || "postgres",
+        database: process.env.DB_NAME || "lib-db",
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 5432,
+        dialect: process.env.DB_DIALECT || "postgres"
+    }
 }
