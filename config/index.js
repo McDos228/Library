1393bbc42521f021953d6ee523d7ad279896d68c
mcdos228 <canyon2011@ukr.net>
2019-04-10 15:17:56 +0300

    [book-manager] update config

--- config/index.js
+++ config/index.js
@@ -3,10 +3,14 @@ module.exports = {
     secret : 'cool-library-secret-key',
     env: process.env.NODE_ENV || 'development',
     database: {
-        username: process.env.DB_USERNAME || "postgres",
-        password: process.env.DB_PASSWORD || "postgres",
-        database: process.env.DB_NAME || "lib-db",
-        host: process.env.DB_HOST || "localhost",
+        username: process.env.DB_USERNAME || "ivtmegtrgcqbqz",
+        // username: process.env.DB_USERNAME || "postgres",
+        password: process.env.DB_PASSWORD || "a2d63f3a20032eabb34df42b98e9e9f7af8c9f380e4d475a3358dae6f8c907af",
+        // password: process.env.DB_PASSWORD || "postgres",
+        database: process.env.DB_NAME || "dbs2eq6gdqo41c",
+        // database: process.env.DB_NAME || "lib-db",
+        host: process.env.DB_HOST || "ec2-54-228-243-238.eu-west-1.compute.amazonaws.com",
+        // host: process.env.DB_HOST || "localhost",
         port: process.env.DB_PORT || 5432,
         dialect: process.env.DB_DIALECT || "postgres"
     }
