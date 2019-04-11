deae2d15b1e6477630d2e9af22f145b2a1a0b21d
mcdos228 <canyon2011@ukr.net>
2019-04-11 15:45:08 +0300

    [book-manager] mf

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
 })
-app.use(cors());
+// app.use(cors());
 // app.use(expressValidator());
 app.use(express.static(__dirname + '/uploads'));
 app.use(bodyParser.json());
