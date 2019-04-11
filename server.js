4ac54c03d58e01aefccefc5d084abd356b564ad9
mcdos228 <canyon2011@ukr.net>
2019-04-11 16:52:13 +0300

    [book-manager] mf

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
 
+var whitelist = ['http://localhost:3000', 'http://example2.com']
 var corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
   }
+}
 
 // app.use(function (req, res, next) {
 //     res.header('Access-Control-Allow-Origin', '*')
