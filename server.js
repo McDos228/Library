4a95c1aa3a0b820312ccb676f7b73a12779e4784
mcdos228 <canyon2011@ukr.net>
2019-04-12 09:19:52 +0300

    [book-manager] mf

--- server.js
+++ server.js
@@ -7,15 +7,13 @@ const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
 
-
-
 app.use(cors());
-app.use( (req, res, next)=> {
+app.use((req, res, next)=> {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     next()
 })
-// app.use(expressValidator());
+
 app.use(express.static(__dirname + '/uploads'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
